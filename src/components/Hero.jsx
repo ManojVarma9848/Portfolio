1c76615e8e111d6ec1138e41d67c93985a0bb2a6
ManojVarma9848 <battalumanojvarma9848@gmail.com>
2024-06-20 19:07:24 +0530

    2nd commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,63 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/rithik.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
+              Manoj Varma
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-400 via-slate-100 to-purple-300 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
+              Full Stack Developer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilePic}
+              alt="Manoj Varma"
+              className="h-64 w-64 object-cover rounded-full" // Adjusted height and added width and rounded-full
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,65 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `My journey as a developer is driven by a deep passion for crafting seamless, dynamic web experiences. With a strong command of React.js, I bring interactive user interfaces to life, while my expertise in MongoDB allows me to design robust and efficient databases. My foundational skills in HTML, CSS, and JavaScript enable me to create visually appealing and responsive designs. I've also integrated advanced functionalities using Firebase for backend services and Stripe for secure payment processing. My proficiency with Git and GitHub ensures that my projects are well-managed and collaborative.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Electronics And Communication Engineering",
+    company: "KL University",
+    description: `I am currently in the third year of my Bachelor's degree in Electronics and Communication Engineering (ECE), maintaining an impressive CGPA of 8.97. My studies so far have provided me with a solid understanding of both theoretical concepts and practical applications in the field.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2020 - 2022",
+    role: "Bipc",
+    company: "Sri Chaitanya Junior Collage",
+    description: `
+I have completed my intermediate education in BiPC with a CGPA of 8.13 and have gained additional proficiency in mathematics and various technologies. My strong foundation in these subjects has equipped me with analytical and problem-solving skills.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2010 - 2020",
+    role: "10/10(CGPA)",
+    company: "Bhashyam High School",
+    description: `
+In my 10th standard, I achieved a perfect CGPA of 10/10, showcasing my academic excellence. Alongside my studies, I actively participated in sports and developed strong skills in teamwork, leadership, and discipline. Excelling in both academics and sports taught me valuable lessons in time management, determination, and maintaining a healthy balance between studies and extracurricular activities.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+ 
+];
+
+export const PROJECTS = [
+  {
+    title: "FoodiHub Website",
+    image: project1,
+    description:
+      "I recently completed a project using HTML, CSS, and JavaScript, integrated with MongoDB and Firebase. This project showcases my proficiency in web development and database management. With sleek animations and seamless functionality, it offers users an engaging experience. This project has enriched my skills in front-end design, back-end integration, and database management, setting a strong foundation for my career in web development.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB,"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Meesho-Inspired E-Commerce Platform",
+    image: project3,
+    description:
+      "I developed a Meesho-inspired e-commerce platform using HTML and CSS. The platform mimics the layout and functionality of Meesho, offering users a similar shopping experience. Key features include product display, navigation, and shopping functionalities, just like in the original Meesho app.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "45-12-5/1 Vijayawada, India, 520001 ",
+  phoneNo: "+91 98488 53325 ",
+  email: "battulamanojvarma9848@gmail.com",
+};
